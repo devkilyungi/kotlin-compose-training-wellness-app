738ddc0f3baefe8a154abc4d9746d9276b0f9afc
devkilyungi <victorkilyungi@gmail.com>
2023-06-30 19:01:41 +0300

    added layout composables

--- app/src/main/java/com/example/thirtydaysofwellness/ExercisesScreen.kt
+++ app/src/main/java/com/example/thirtydaysofwellness/ExercisesScreen.kt
@@ -1,4 +1,88 @@
 package com.example.thirtydaysofwellness
 
-class ExercisesScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.thirtydaysofwellness.model.DailyExercise
+
+@Composable
+fun DailyExerciseList(dailyExercises: List<DailyExercise>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        itemsIndexed(dailyExercises) { _: Int, dailyExercise: DailyExercise ->
+            DailyExerciseCard(
+                dailyExercise = dailyExercise,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium),
+                    vertical = dimensionResource(
+                        id = R.dimen.padding_small
+                    )
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun DailyExerciseCard(
+    dailyExercise: DailyExercise,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            Text(
+                text = stringResource(id = dailyExercise.day),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = stringResource(id = dailyExercise.exerciseTitle),
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_medium)))
+            Box(
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                Image(
+                    painter = painterResource(id = dailyExercise.image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_medium)))
+            Text(
+                text = stringResource(id = dailyExercise.exerciseDescription),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
 }
