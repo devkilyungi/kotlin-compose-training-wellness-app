ba868e54d10a6fb83e18a51d44797c8d554c8677
devkilyungi <victorkilyungi@gmail.com>
2023-06-30 19:01:07 +0300

    code formatting

--- app/src/main/java/com/example/thirtydaysofwellness/MainActivity.kt
+++ app/src/main/java/com/example/thirtydaysofwellness/MainActivity.kt
@@ -1,14 +1,23 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.thirtydaysofwellness
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.thirtydaysofwellness.model.ExerciseDatasource
 import com.example.thirtydaysofwellness.ui.theme.ThirtyDaysOfWellnessTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +39,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ThirtyDaysOfWellnessApp() {
+    Scaffold(
+        topBar = {
+            ThirtyDaysOfWellnessTopAppBar()
+        }
+    ) { innerPadding ->
+        DailyExerciseList(
+            ExerciseDatasource.dailyExercises,
+            Modifier.padding(innerPadding)
+        )
+    }
+}
 
+@Composable
+fun ThirtyDaysOfWellnessTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
